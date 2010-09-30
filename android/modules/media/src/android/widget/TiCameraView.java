56c09f801257ae7a8487fa68d2c9aa6dfaffae9b
cfenzo <enzo@webfreak.no>
2010-09-30 10:43:18 +0200

    Refactored TiCameraView widget Ready for MediaModule refactoring

--- android/modules/media/src/android/widget/TiCameraView.java
+++ android/modules/media/src/android/widget/TiCameraView.java
@@ -1,5 +1,12 @@
 package android.widget;
 
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
 public class TiCameraView extends SurfaceView
 {
    Camera camera;
@@ -15,30 +22,29 @@ public class TiCameraView extends SurfaceView
 	   previewHolder = this.getHolder();
 	   previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 	   previewHolder.addCallback(surfaceHolderListener);
-            
-	   SurfaceHolder.Callback surfaceHolderListener = new SurfaceHolder.Callback() {
-		   public void surfaceCreated(SurfaceHolder holder) {
-      	            camera=Camera.open();
-      	            try {
-      	                    camera.setPreviewDisplay(previewHolder);
-      	            }
-      	            catch (Throwable ){ }
-      	           }
-      	   public void surfaceChanged(SurfaceHolder holder, int format, int width,
-      	         int height)
-      	   {
-      	      Parameters params = camera.getParameters();
-      	      params.setPreviewSize(w, h);
-      	      params.setPictureFormat(PixelFormat.JPEG);
-      	              camera.setParameters(params);
-      	              camera.startPreview();
-      	   }
-
-      	   public void surfaceDestroyed(SurfaceHolder arg0)
-      	   {
-      	      camera.stopPreview();
-      	      camera.release();   
-      	   }
-	   };
 	}
+   
+   SurfaceHolder.Callback surfaceHolderListener = new SurfaceHolder.Callback() {
+	   public void surfaceCreated(SurfaceHolder holder) {
+  	            camera=Camera.open();
+  	            try {
+  	                    camera.setPreviewDisplay(previewHolder);
+  	            }
+  	            catch (Throwable t){ }
+  	           }
+  	   public void surfaceChanged(SurfaceHolder holder, int format, int w,int h)
+  	   {
+  	      Parameters params = camera.getParameters();
+  	      params.setPreviewSize(w, h);
+  	      params.setPictureFormat(PixelFormat.JPEG);
+  	              camera.setParameters(params);
+  	              camera.startPreview();
+  	   }
+
+  	   public void surfaceDestroyed(SurfaceHolder arg0)
+  	   {
+  	      camera.stopPreview();
+  	      camera.release();   
+  	   }
+   };
 }
